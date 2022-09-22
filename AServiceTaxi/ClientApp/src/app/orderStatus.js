9dc2b25c2765efcf29aecf767419c2400edb7559
pavelDmytrenko <69463126+pavelDmytrenko@users.noreply.github.com>
2022-09-22 22:44:37 +0300

    clean up repo  - js files2

--- AServiceTaxi/ClientApp/src/app/orderStatus.js
+++
@@ -1,7 +0,0 @@
-export var OrderStatus;
-(function (OrderStatus) {
-    OrderStatus[OrderStatus["InProgress"] = 1] = "InProgress";
-    OrderStatus[OrderStatus["Waiting"] = 2] = "Waiting";
-    OrderStatus[OrderStatus["Done"] = 3] = "Done";
-})(OrderStatus || (OrderStatus = {}));
-//# sourceMappingURL=orderStatus.js.map
